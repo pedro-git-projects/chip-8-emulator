f673ea3ecd736a731bb213d7395f6b31c5ae9852
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-10-12 14:40:36 -0300

    adding dependencies #1|handling events #2

--- src/main.rs
+++ src/main.rs
@@ -1,2 +1,39 @@
+use sdl2::event::Event;
+use sdl2::keyboard::Keycode;
+use sdl2::pixels::Color;
+use sdl2::rect::Rect;
+
 fn main() {
+    let sdl_context = sdl2::init().unwrap();
+    let video_subsystem = sdl_context.video().unwrap();
+
+    let window = video_subsystem
+        .window("CHIP-8 emulator", 640, 320)
+        .position_centered()
+        .build()
+        .unwrap();
+
+    let mut canvas = window.into_canvas().build().unwrap();
+
+    canvas.set_draw_color(Color::RGBA(0, 0, 0, 0));
+    canvas.clear();
+
+    canvas.set_draw_color(Color::RGBA(255, 255, 255, 0));
+    canvas.fill_rect(Rect::new(0, 0, 40, 40));
+
+    canvas.present();
+
+    let mut event_pump = sdl_context.event_pump().unwrap();
+    'running: loop {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => break 'running,
+                _ => {}
+            }
+        }
+    }
 }
