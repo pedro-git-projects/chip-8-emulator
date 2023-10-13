c4f5224c71980d11669722aa90ed01d136c331d9
pedro-git-projects <pedro.coding.contact@gmail.com>
2023-10-12 21:22:55 -0300

    implementing charset #11

--- src/emulator/chip.rs
+++ src/emulator/chip.rs
@@ -5,12 +5,19 @@ use super::keyboard::Keyboard;
 const MEMORY_CAPACITY: usize = 4096;
 const TOTAL_DATA_REGISTERS: usize = 16;
 const TOTAL_STACK_DEPTH: u8 = 16;
+const CHIP8_DEFAULT_CHARACTER_SET: [u8; 80] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, 0x20, 0x60, 0x20, 0x20, 0x70, 0xF0, 0x10, 0xF0, 0x80, 0xF0, 0xF0,
+    0x10, 0xF0, 0x10, 0xF0, 0x90, 0x90, 0xF0, 0x10, 0x10, 0xF0, 0x80, 0xF0, 0x10, 0xF0, 0xF0, 0x80,
+    0xF0, 0x90, 0xF0, 0xF0, 0x10, 0x20, 0x40, 0x40, 0xF0, 0x90, 0xF0, 0x90, 0xF0, 0xF0, 0x90, 0xF0,
+    0x10, 0xF0, 0xF0, 0x90, 0xF0, 0x90, 0x90, 0xE0, 0x90, 0xE0, 0x90, 0xE0, 0xF0, 0x80, 0x80, 0x80,
+    0xF0, 0xE0, 0x90, 0x90, 0x90, 0xE0, 0xF0, 0x80, 0xF0, 0x80, 0xF0, 0xF0, 0x80, 0xF0, 0x80, 0x80,
+];
 
 // unsigend short = u16
 // unsigend char = u8
 
 pub struct Chip8 {
-    memory: [char; MEMORY_CAPACITY],
+    memory: [u8; MEMORY_CAPACITY],
     v: [char; TOTAL_DATA_REGISTERS], // Chip-8 has 16 general purpose 8-bit registers, usually referred to as Vx, where x is a hexadecimal digit (0 through F).
     i: u16, // There is also a 16-bit register called I. This register is generally used to store memory addresses, so only the lowest (rightmost) 12 bits are usually used.
     delay_timer: u8,
@@ -23,7 +30,7 @@ pub struct Chip8 {
 
 impl Chip8 {
     pub fn new() -> Chip8 {
-        let memory = ['\0'; MEMORY_CAPACITY];
+        let memory = [0; MEMORY_CAPACITY];
         let v = ['\0'; TOTAL_DATA_REGISTERS];
         let i = 0;
         let delay_timer = 0;
@@ -33,7 +40,7 @@ impl Chip8 {
         let stack = ['\0'; TOTAL_STACK_DEPTH as usize];
         let keyboard = Keyboard::new();
 
-        Chip8 {
+        let mut chip8 = Chip8 {
             memory,
             v,
             i,
@@ -43,10 +50,14 @@ impl Chip8 {
             stack_pointer,
             stack,
             keyboard,
+        };
+        for (i, &value) in CHIP8_DEFAULT_CHARACTER_SET.iter().enumerate() {
+            chip8.memory[i] = value;
         }
+        chip8
     }
 
-    pub fn set_memory_addr(&mut self, index: usize, value: char) -> Result<(), &str> {
+    pub fn set_memory_addr(&mut self, index: usize, value: u8) -> Result<(), &str> {
         match index.cmp(&MEMORY_CAPACITY) {
             Ordering::Less => {
                 self.memory[index] = value;
@@ -56,7 +67,7 @@ impl Chip8 {
         }
     }
 
-    pub fn get_memory_addr(&self, index: usize) -> Result<char, &str> {
+    pub fn get_memory_addr(&self, index: usize) -> Result<u8, &str> {
         match index.cmp(&MEMORY_CAPACITY) {
             Ordering::Less => Ok(self.memory[index]),
             _ => Err("memory out of bounds!"),
@@ -94,7 +105,7 @@ mod tests {
     fn test_set_memory_addr_within_bounds() {
         let mut chip8 = Chip8::new();
         let index = 0;
-        let value = 'A';
+        let value = 0x90;
         let result = chip8.set_memory_addr(index, value);
         assert_eq!(result, Ok(()));
     }
@@ -103,7 +114,7 @@ mod tests {
     fn test_set_memory_addr_out_of_bounds() {
         let mut chip8 = Chip8::new();
         let index = MEMORY_CAPACITY + 1;
-        let value = 'A';
+        let value = 0x90;
         let result = chip8.set_memory_addr(index, value);
         assert_eq!(result, Err("memory out of bounds!"));
     }
@@ -112,9 +123,9 @@ mod tests {
     fn test_get_memory_addr_within_bounds() {
         let mut chip8 = Chip8::new();
         let index = 0;
-        chip8.set_memory_addr(index, 'A').unwrap();
+        chip8.set_memory_addr(index, 0x90).unwrap();
         let result = chip8.get_memory_addr(index);
-        assert_eq!(result, Ok('A'));
+        assert_eq!(result, Ok(0x90));
     }
 
     #[test]
